29e15ac74167f7d3fd4b0278f5d26ade9f585c5f
Isak <126728670+Iz465@users.noreply.github.com>
2024-10-04 14:59:36 +1300

    more flask teachings added

---
+++ importing_3_html.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, make_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('setcookie.html')
+
+@app.route('/setcookie', methods = ["POST", "GET"])
+def setcookie():
+    if request.method == "POST":
+        username = request.form['nm']
+        resp = make_response(render_template('readcookie.html'))
+        resp.set_cookie("username", username)
+
+        age = request.form["age"]
+        resp.set_cookie("age", age)
+
+        return resp
+@app.route('/getcookie')
+def getcookie():
+    name=request.cookies.get('username')
+    age = request.cookies.get('age')
+    return f"hello {name}, your age is {age}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
